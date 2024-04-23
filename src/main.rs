d3bbfa9aef4af74568a9e6419c485ba9461b6719
Blaz Bratanic <blaz.bratanic@gmail.com>
2024-04-23 21:41:59 +0200

    activations

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-mod math;
+mod nn;
 
 fn main() {
-    let ref m1 = math::Matrix::<f32>::new(&[1, 2, 3]);
-    let ref m2 = math::Matrix::<f32>::with_value(&[1, 2, 3], 5.0);
-    let ref m3 = math::Matrix::<f32>::with_value(&[1, 2, 3], 2.0);
+    let ref m1 = nn::Matrix::<f32>::new(&[1, 2, 3]);
+    let ref m2 = nn::Matrix::<f32>::with_value(&[1, 2, 3], 5.0);
+    let ref m3 = nn::Matrix::<f32>::with_value(&[1, 2, 3], 2.0);
     println!("{}", m1);
     println!("{}", m2);
     println!("{}", m1 + m2);
@@ -12,11 +12,11 @@ fn main() {
     println!("{}", m3 * 5.0);
     println!("{}", (m3 * 5.0 + 9.0) / 2.0);
 
-    let ref m4 = math::Matrix::<f32>::with_value(&[8, 4], 8.0);
-    let ref m5 = math::Matrix::<f32>::with_value(&[4, 9], 5.0);
-    println!("{:?}", math::matmul(m4, m5));
+    let ref m4 = nn::Matrix::<f32>::with_value(&[8, 4], 8.0);
+    let ref m5 = nn::Matrix::<f32>::with_value(&[4, 9], 5.0);
+    println!("{:?}", nn::matmul(m4, m5));
 
-    let ref m6 = math::Matrix::<f32>::with_value(&[2, 8, 4], 8.0);
-    let ref m7 = math::Matrix::<f32>::with_value(&[2, 4, 9], 5.0);
-    println!("{}", math::matmul(m6, m7));
+    let ref m6 = nn::Matrix::<f32>::with_value(&[2, 8, 4], 8.0);
+    let ref m7 = nn::Matrix::<f32>::with_value(&[2, 4, 9], 5.0);
+    println!("{}", nn::matmul(m6, m7));
 }
